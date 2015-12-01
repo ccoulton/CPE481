5fda81e955aff92cf49713867b3f102813197c14
ccoulton <coultonc@gmail.com>
2015-12-01 12:16:07 -0800

    exported samusjump

---
+++ source/samusJump.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(samusJump)
+
+//======================================================================
+//
+//	samusJump, 32x1152@4, 
+//	+ palette 256 entries, not compressed
+//	+ 576 tiles Metatiled by 4x4 not compressed
+//	Total size: 512 + 18432 = 18944
+//
+//	Time-stamp: 2015-12-01, 12:12:16
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_SAMUSJUMP_H
+#define GRIT_SAMUSJUMP_H
+
+#define samusJumpTilesLen 18432
+extern const unsigned int samusJumpTiles[4608];
+
+#define samusJumpPalLen 512
+extern const unsigned int samusJumpPal[128];
+
+#endif // GRIT_SAMUSJUMP_H
+
+//}}BLOCK(samusJump)
